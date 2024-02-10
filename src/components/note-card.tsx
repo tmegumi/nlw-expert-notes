c797a0d054c332e5e3c95ce700d995e211049174
tmegumi <tha.megumi@gmail.com>
2024-02-10 09:43:19 -0300

    chore: format date note

--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -15,7 +15,10 @@ export function NoteCard({ note }: NoteCardProps) {
     <Dialog.Root>
       <Dialog.Trigger className="rounded-md text-left flex flex-col bg-slate-800 p-5 gap-y-3 overflow-hidden outline-none relative hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
         <span className="text-sm font-medium text-slate-300">
-          {note.date.toISOString()}
+          {formatDistanceToNow(note.date, {
+            locale: ptBR,
+            addSuffix: true,
+          })}
         </span>
         <p className="text-sm leading-6 text-slate-400">{note.content}</p>
 
